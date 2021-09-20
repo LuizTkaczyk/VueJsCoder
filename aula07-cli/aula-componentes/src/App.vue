1c5e1dcb3195b82dc95db1a4c872d8a4860cc3fb
LuizTkaczyk <luiz8907@hotmail.com>
2021-09-20 20:15:07 -0300

    Desafio-Components

--- aula07-cli/aula-componentes/src/App.vue
+++ aula07-cli/aula-componentes/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
    
     <app-contadores></app-contadores>
+    <button>??</button>
     
    
   </div>
--- aula07-cli/aula-componentes/src/Contadores.vue
+++ aula07-cli/aula-componentes/src/Contadores.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="contadores">
-       <h1>Contadores</h1>
-      <app-contador v-for="c in 3" :key="c"></app-contador>
+    <h1>Contadores</h1>
+    <!-- usando v-for para repetir n de vezes -->
+    <app-contador v-for="c in 3" :key="c"></app-contador>
   </div>
 </template>
 
@@ -9,9 +10,13 @@
 // Criando um componente local
 import Contador from "./Contador.vue";
 export default {
+  // app-contador é a tag usada no App.vue para referanciar o componente criado
   components: { "app-contador": Contador },
 };
 </script>
 
-<style>
+<style scoped>
+div {
+  border: 1px solid red;
+}
 </style>
