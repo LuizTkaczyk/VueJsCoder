08f9f4b6a8f4cefb8aa63a9c85e05755343b8fa9
LuizTkaczyk <luiz8907@hotmail.com>
2021-09-23 19:51:36 -0300

    App todo completo

---
+++ aula09-todo/tarefas/src/components/Task.vue
@@ -0,0 +1,72 @@
+<template>
+  <div @click="$emit('taskStateChanged', task)" class="task" :class="stateClass">
+    <span class="close" @click.stop="$emit('taskDeleted', task)">x</span>
+    <p>{{ task.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: { type: Object, required: true },
+  },
+  computed: {
+    stateClass() {
+      return {
+        //adicionando uma classe a task, como pending ou done, desse modo adicionando estilos deferentes
+        pending: this.task.pending,
+        done: !this.task.pending,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+.task {
+  position: relative;
+  box-sizing: border-box;
+  width: 350px;
+  height: 150px;
+  padding: 10px;
+  border-radius: 8px;
+  font-size: 2rem;
+  font-weight: 300;
+  cursor: pointer;
+  user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.pending {
+  border-left: 12px solid rgb(112, 59, 59);
+  background-color: red;
+}
+.done {
+  color: white;
+  border-left: 12px solid green;
+  background-color: goldenrod;
+  text-decoration: line-through;
+}
+
+.pending .close {
+  background-color: red;
+}
+.done .close {
+  background-color: rgb(143, 97, 11);
+}
+.close {
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  font-size: 0.9rem;
+  font-weight: 600;
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+
+}
+</style>
