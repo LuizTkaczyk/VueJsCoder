3db14076cc96ad3a8d8343daf061f2f877341587
LuizTkaczyk <luiz8907@hotmail.com>
2021-09-30 09:36:58 -0300

    Requisicoes http Axios

--- aula14-requisicoes/http-exercicios/src/App.vue
+++ aula14-requisicoes/http-exercicios/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app" class="container">
     <h1>HTTP com Axios</h1>
+    <!-- alert da mensagem de sucesso ao terminar o cadastro com sucesso -->
+    <b-alert
+      show
+      dismissible
+      v-for="mensagem in mensagens"
+      :key="mensagem.texto"
+      :variant="mensagem.tipo"
+      >{{ mensagem.texto }}</b-alert
+    >
     <b-card>
       <b-form-group label="Nome:">
         <b-form-input
@@ -31,6 +40,12 @@
         <strong>Nome:</strong>{{ usuario.nome }}<br />
         <strong>E-mail:</strong>{{ usuario.email }}<br />
         <strong>ID:</strong>{{ id }}<br />
+        <b-button variant="warning" size="lg" @click="carregar(id)"
+          >Carregar</b-button
+        >
+        <b-button variant="danger" size="lg" class="ml-2" @click="excluir(id)"
+          >Excluir</b-button
+        >
       </b-list-group-item>
     </b-list-group>
   </div>
@@ -46,7 +61,9 @@ export default {
   // }
   data() {
     return {
+      mensagens: [],
       usuarios: [],
+      id: null,
       usuario: {
         nome: "",
         email: "",
@@ -54,18 +71,45 @@ export default {
     };
   },
   methods: {
+    limpar() {
+      this.usuario.nome = "";
+      this.usuario.email = "";
+      this.id = null;
+      this.mensagens = [];
+    },
+    carregar(id) {
+      this.id = id;
+      this.usuario = { ...this.usuarios[id] };
+    },
+    excluir(id) {
+      this.$http.delete(`/usuarios/${id}.json`).then(() => this.limpar()).catch(err =>{
+        this.limpar()
+        this.mensagens.push({
+          texto:'Problema ao excluir o cadastro',
+          tipo:'danger'
+        })
+      });
+    },
     //salva no banco de dados e depois limpa os campos do formulário
     salvar() {
-      this.$http.post("usuarios.json", this.usuario).then((resposta) => {
-        this.usuario.nome = "";
-        this.usuario.email = "";
+      //this.$http.post("usuarios.json", this.usuario).then(()=> this.limpar());//chamando a função para limpar os campos
+      const metodo = this.id ? "patch" : "post";
+      const finalUrl = this.id ? `/${this.id}.json` : ".json";
+
+      this.$http[metodo](`/usuarios${finalUrl}`, this.usuario).then((_) => {
+        this.limpar();
+        //exibindo uma mesagem de sucesso ao terminar o cadastro
+        this.mensagens.push({
+          texto: "Cadastrado com sucesso",
+          tipo: "success",
+        });
       });
     },
     //no caso de pegar os dados do banco de dados o .get não é obrigatorio
     obterUsuarios() {
       this.$http.get("usuarios.json").then((resposta) => {
         this.usuarios = resposta.data;
-		//console.log(resposta.data)
+        //console.log(resposta.data)
       });
     },
   },
