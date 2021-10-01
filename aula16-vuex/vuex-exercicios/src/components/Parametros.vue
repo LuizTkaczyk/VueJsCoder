ea05ed73d32d31d0aa58af8e8e44817b2f093802
LuizTkaczyk <luiz8907@hotmail.com>
2021-10-01 18:27:58 -0300

    Vuex

--- aula16-vuex/vuex-exercicios/src/components/Parametros.vue
+++ aula16-vuex/vuex-exercicios/src/components/Parametros.vue
@@ -1,32 +1,37 @@
 <template>
-    <Painel titulo="Parâmetros" vermelho>
-        <div class="parametros">
-            <span>
-                <strong>Quantidade Padrão: </strong> 
-                <input type="number" v-model="quantidade">
-            </span>
-            <span>
-                <strong>Preço Padrão: </strong>
-                <input type="number" v-model="preco">
-            </span>
-        </div>
-    </Painel>
+  <Painel titulo="Parâmetros" vermelho>
+    <div class="parametros">
+      <span>
+        <strong>Quantidade Padrão: </strong>
+        <input type="number" v-model="quantidade" />
+      </span>
+      <span>
+        <strong>Preço Padrão: </strong>
+        <input type="number" v-model="preco" />
+      </span>
+    </div>
+  </Painel>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            quantidade: 0,
-            preco: 0
-        }
-    }
-}
+  //propriedades computadas apenas recebem valores!!!!!!!!!!!!!!!!!!!
+  computed: {
+    quantidade: {
+      get() {
+        return this.$store.state.quantidade;
+      },
+      set(valor) {
+        this.$store.commit("setQuantidade", valor);
+      },
+    },
+  },
+};
 </script>
 
 <style>
-    .parametros {
-        display: flex;
-        justify-content: space-around;
-    }
+.parametros {
+  display: flex;
+  justify-content: space-around;
+}
 </style>
--- aula16-vuex/vuex-exercicios/src/store/store.js
+++ aula16-vuex/vuex-exercicios/src/store/store.js
@@ -9,7 +9,9 @@ export default new Vuex.Store({
         //     { id: 1, nome: 'Produto 1', quantidade: 7, preco: 14.55 },
         //     { id: 2, nome: 'Produto 2', quantidade: 10, preco: 22.99 },
         //    { id: 3, nome: 'Produto 3', quantidade: 1, preco: 43.18 },
-        ]
+        ],
+        quantidade:0,
+        preco:0
 
     },
     getters:{
@@ -20,6 +22,12 @@ export default new Vuex.Store({
     mutations:{//ou set
         adicionarProduto(state, produto){
             state.produtos.push(produto)
+        },
+        setQuantidade(state, payload){
+            state.quantidade = payload
+        },
+        setPreco(state, payload){
+            state.preco = payload
         }
     },
     actions:{
