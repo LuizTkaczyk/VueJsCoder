f848d0ce76d3c3b2152b8605e9423b10db6044dd
LuizTkaczyk <luiz8907@hotmail.com>
2021-10-04 17:01:40 -0300

    Projeto1

---
+++ aula17-projeto1/stock-trader/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+	  <!-- O cabeçalho irá aparecer em todas as páginas -->
+	  <Header></Header>
+    <v-content>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+export default {
+	components:{ Header },
+  created(){
+    this.$store.dispatch('initStocks')
+  }
+};
+</script>
+
+<style>
+</style>
+
---
+++ aula17-projeto1/stock-trader/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase mr-4">
+      <span>Stock</span>
+      <span class="font-weight-light">Trader</span>
+    </v-toolbar-title>
+    <v-toolbar-items>
+      <!-- Usando a propriedade -to- não é preciso do router link !!!!!!!!!! -->
+      <v-btn flat to="/"> Inicio</v-btn>
+      <v-btn flat to="/portfolio">Portfólio</v-btn>
+      <v-btn flat to="/stocks">Ações</v-btn>
+    </v-toolbar-items>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+      <v-btn flat>Finalizar o dia</v-btn>
+      <v-menu offset-y>
+        <!-- Quando clicado em 'Salvar e Carregar' abre um menu com as opções abaixo -->
+        <v-btn flat slot="activator">Salvar e Carregar</v-btn>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title>Salvar Dados</v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-title>Carregar Dados</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
