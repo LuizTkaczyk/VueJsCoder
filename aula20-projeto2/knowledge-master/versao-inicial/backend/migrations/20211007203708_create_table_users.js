50cc05af6d709f26d9a712d15df81fe998c3354f
LuizTkaczyk <luiz8907@hotmail.com>
2021-10-08 20:12:43 -0300

    Projeto 2 - backend

--- aula20-projeto2/knowledge-master/versao-inicial/backend/migrations/20211007203708_create_table_users.js
+++ aula20-projeto2/knowledge-master/versao-inicial/backend/migrations/20211007203708_create_table_users.js
@@ -1,8 +1,18 @@
 
+//criando a tabela no método up!!!!!!!!!!!!!!!!!!!
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', table =>{
+        table.increments('id').primary()
+        table.string('name').notNull()
+        table.string('email').notNull()
+        table.string('password').notNull()
+        table.boolean('admin').notNull().default(false)
+
+    })
   
 };
 
+//criando a tabela no método down de modo oposto!!!!!!!!!!!!!!!!!!!
 exports.down = function(knex, Promise) {
-  
-};
+  return knex.schema.dropTable('users')
+}
