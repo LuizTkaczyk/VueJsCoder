63b0a255db6d7185b0fbcb62aabae7534d26a388
LuizTkaczyk <luiz8907@hotmail.com>
2021-10-18 11:53:50 -0300

    Inicio front

--- aula20-projeto2/knowledge-master/versao-inicial/frontend/src/App.vue
+++ aula20-projeto2/knowledge-master/versao-inicial/frontend/src/App.vue
@@ -1,15 +1,44 @@
 <template>
-	<div id="app">
-		<h1>Versão Inicial</h1>
-	</div>
+  <div id="app">
+    <Header title="Cod3r - Projeto 2" :hideToggle="false"></Header>
+    <Menu></Menu>
+    <Content></Content>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Header from "./components/template/Header.vue";
+import Menu from "./components/template/Menu.vue";
+import Footer from "./components/template/Footer.vue";
+import Content from "./components/template/Content.vue";
+
 export default {
-	name: "App",
-}
+  components: { Header, Menu, Footer, Content },
+  name: "App",
+};
 </script>
 
 <style>
 
+*{
+	font-family: "Lato", sans-serif;
+
+}
+
+body{
+	margin: 0;
+
+}
+
+#app{
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: graycale;
+
+	height: 100vh;
+	display: grid;
+	grid-template-rows: 60px 1fr 40px;
+	grid-template-columns: 300px 1fr;
+	grid-template-areas: "header header" "menu content"  "menu footer";
+}
 </style>
---
+++ aula20-projeto2/knowledge-master/versao-inicial/frontend/src/components/template/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+  <aside class="menu"></aside>
+</template>
+
+<script>
+export default {
+name:'Menu'
+}
+</script>
+
+<style>
+    .menu{
+        grid-area: menu;
+        background: linear-gradient(to right, #232526, #414345);
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: column;
+    }
+
+</style>
