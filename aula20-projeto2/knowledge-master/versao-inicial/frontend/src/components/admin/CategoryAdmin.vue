7fdaf478b90184777de30a623226ffaeaf633edd
LuizTkaczyk <luiz8907@hotmail.com>
2021-10-22 15:55:19 -0300

    Projeto2 articles

--- aula20-projeto2/knowledge-master/versao-inicial/frontend/src/components/admin/CategoryAdmin.vue
+++ aula20-projeto2/knowledge-master/versao-inicial/frontend/src/components/admin/CategoryAdmin.vue
@@ -1,73 +1,53 @@
 <template>
   <div class="category-admin">
     <b-form>
-      <input type="hidden" id="category-id" v-model="category.id" />
-
-      <b-row>
-        <b-col xs="12">
-          <b-form-group label="Nome:" label-for="category-name">
-            <b-form-input
-              id="category-name"
-              type="text"
-              v-model="category.name"
-              required
-              placeholder="Informe o Nome da Categoria..."
-              :readonly="mode === 'remove'"
-            ></b-form-input>
-          </b-form-group>
-        </b-col>
-        </b-row>
-
-
-      <b-row v-if="mode === 'save' ">
-        <b-col xs="12">
-          <b-form-group label="Categoria pai" label-for="category-parentId">
-            <b-form-select id="category-parentId" :options="categories" v-model="category.parentId"></b-form-select>
-          </b-form-group>
-        </b-col>
-       
-      </b-row>
-
-      <b-row v-else>
-        <b-col xs="12">
-          <b-form-group label="Categoria pai" label-for="category-parentId">
-           <b-form-input
-              id="category-name"
-              type="text"
-              v-model="category.path"
-              required
-             
-              readonly
-            ></b-form-input>
-          </b-form-group>
-        </b-col>
-       
-      </b-row>
-
-
-    
-
-      <b-row>
-        <b-col xs="12">
-          <b-button variant="primary" v-if="mode == 'save'" @click="save"
-            >Salvar</b-button
-          >
-          <b-button variant="danger" v-if="mode == 'remove'" @click="remove"
-            >Excluir</b-button
-          >
-          <b-button class="ml-2" @click="reset">Cancelar</b-button>
-        </b-col>
-      </b-row>
+      <input id="category-id" type="hidden" v-model="category.id" />
+      <b-form-group label="Nome:" label-for="category-name">
+        <b-form-input
+          id="category-name"
+          type="text"
+          v-model="category.name"
+          required
+          :readonly="mode === 'remove'"
+          placeholder="Informe o Nome da Categoria..."
+        />
+      </b-form-group>
+      <b-form-group label="Categoria Pai:" label-for="category-parentId">
+        <b-form-select
+          v-if="mode === 'save'"
+          id="category-parentId"
+          :options="categories"
+          v-model="category.parentId "
+        />
+        <b-form-input
+          v-else
+          id="category-parentId"
+          type="text"
+          v-model="category.path"
+          readonly
+        />
+      </b-form-group>
+      <b-button variant="primary" v-if="mode === 'save'" @click="save"
+        >Salvar</b-button
+      >
+      <b-button variant="danger" v-if="mode === 'remove'" @click="remove"
+        >Excluir</b-button
+      >
+      <b-button class="ml-2" @click="reset">Cancelar</b-button>
     </b-form>
     <hr />
     <b-table hover striped :items="categories" :fields="fields">
       <template slot="actions" slot-scope="data">
-        <b-button variant="warning" @click="loadCategory(data.item)" class="mr-2"
-          ><i class="fa fa-pencil"></i
-        ></b-button>
-        <b-button variant="danger" @click="loadCategory(data.item, 'remove')"
-          ><i class="fa fa-trash"></i
-        ></b-button>
+        <b-button
+          variant="warning"
+          @click="loadCategory(data.item)"
+          class="mr-2"
+        >
+          <i class="fa fa-pencil"></i>
+        </b-button>
+        <b-button variant="danger" @click="loadCategory(data.item, 'remove')">
+          <i class="fa fa-trash"></i>
+        </b-button>
       </template>
     </b-table>
   </div>
@@ -87,7 +67,7 @@ export default {
         { key: "id", label: "Código", sortable: true }, //comm sortable é possível fazer a ordenação dos nomes da tabela
         { key: "name", label: "Nome", sortable: true },
         { key: "path", label: "Caminho", sortable: true },
-    
+
         { key: "actions", label: "Ações" },
       ],
     };
@@ -96,11 +76,11 @@ export default {
   methods: {
     loadCategories() {
       const url = `${baseApiUrl}/categories`;
-      axios.get(url).then((res) => {
+      axios.get(url).then(res => {
         //this.categories = res.data;
-        this.categories = res.data.map(category =>{
-          return {...category, value:category.id, text:category.path}
-        })
+        this.categories = res.data.map(category => {
+          return { ...category, value: category.id, text: category.path };
+        });
       });
     },
 
